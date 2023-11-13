849b9c60035b0f2da736fc9f45d79d00fe3e923e
Simon Boylan <siboy73@gmail.com>
2023-11-13 11:27:26 +0000

    Adjust how image is drawn to keep aspect ratio

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,7 @@
-
-var friendly = document.getElementById("friendly")
-
-
-friendly.onload = function () {
 const canvas = document.getElementById("alienGame");
 const ctx = canvas.getContext("2d");
-ctx.drawImage(friendly, 143, 130, 15, 15);
+
+window.onload = function () {
+  let friendly = document.getElementById("friendly");
+  ctx.drawImage(friendly, 143, 125, friendly.width / 35, friendly.height / 35);
 }
-
-
-
-
-
