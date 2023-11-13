89ac224ebc61f07d6c5e505e50abff0b4051b923
Simon Boylan <siboy73@gmail.com>
2023-11-13 12:09:21 +0000

    Add a game loop to refresh the image

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,11 @@
 const canvas = document.getElementById("alienGame");
 const ctx = canvas.getContext("2d");
 
-window.onload = function () {
-  let friendly = document.getElementById("friendly");
+let friendly = document.getElementById("friendly");
+
+function game() {
   ctx.drawImage(friendly, 143, 125, friendly.width / 35, friendly.height / 35);
 }
+
+setInterval(game, 1000/60);
+
