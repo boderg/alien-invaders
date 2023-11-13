48528eec97693c710e026ba00b31ad61ab4fa11b
Simon Boylan <siboy73@gmail.com>
2023-11-13 10:15:35 +0000

    Draw friendly image onto canvas

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+
+var friendly = document.getElementById("friendly")
+
+
+friendly.onload = function () {
+const canvas = document.getElementById("alienGame");
+const ctx = canvas.getContext("2d");
+ctx.drawImage(friendly, 143, 130, 15, 15);
+}
+
+
+
+
+
