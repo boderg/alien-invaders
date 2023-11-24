0ff7673d411380f964ad51d013219dbd49995f3b
Simon Boylan <siboy73@gmail.com>
2023-11-24 10:42:53 +0000

    Tidy working code and remove duplicated code

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,32 +6,19 @@ let friendly = document.getElementById("friendly");
 let friendlyX = 143; // initial x position
 let speed = 5; // speed of movement
 
-
-window.addEventListener('keydown', function(e) {
-  switch(e.key) {
-    case 'ArrowLeft':
-      friendlyX -= speed; // move left
-      break;
-    case 'ArrowRight':
-      friendlyX += speed; // move right
-      break;
-  }
-});
-
-function startGame() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height); // clear the canvas
-  ctx.drawImage(friendly, friendlyX, 125, friendly.width / 35, friendly.height / 35);
-}
-
 let projectiles = []; // array to hold projectiles
 
 window.addEventListener('keydown', function(e) {
   switch(e.key) {
     case 'ArrowLeft':
-      friendlyX -= speed; // move left
+      if (friendlyX - speed >= 0) { // check if friendly is within the left edge of the canvas
+        friendlyX -= speed; // move left
+      }
       break;
     case 'ArrowRight':
-      friendlyX += speed; // move right
+      if (friendlyX + speed <= canvas.width - friendly.width / 35) { // check if friendly is within the right edge of the canvas
+        friendlyX += speed; // move right
+      }
       break;
     case ' ':
       // fire a projectile
@@ -43,13 +30,13 @@ window.addEventListener('keydown', function(e) {
 function startGame() {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // clear the canvas
 
-  // draw and move projectiles
-  for(let i = 0; i < projectiles.length; i++) {
-    let projectile = projectiles[i];
-    projectile.y -= 5; // move the projectile up
-    ctx.fillStyle = 'rgb(167, 249, 2)'; // set the color
-    ctx.fillRect(projectile.x, projectile.y, 1, 4); // draw the projectile
-  }
+// draw and move projectiles
+for(let i = 0; i < projectiles.length; i++) {
+  let projectile = projectiles[i];
+  projectile.y -= 5; // move the projectile up
+  ctx.fillStyle = 'rgb(167, 249, 2)'; // set the color
+  ctx.fillRect(projectile.x, projectile.y, 1, 4); // draw the projectile
+}
 
   ctx.drawImage(friendly, friendlyX, 125, friendly.width / 35, friendly.height / 35);
 }
